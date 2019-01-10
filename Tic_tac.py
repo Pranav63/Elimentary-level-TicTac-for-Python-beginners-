92ce3df3a9dd008b3a45204cdc0ff7ff77481694
Pranav Arora <pranav2vis@gmail.com>
2019-01-11 03:09:47 +0800

    Update Tic_tac.py

--- Tic_tac.py
+++ Tic_tac.py
@@ -8,4 +8,13 @@ def print_borad():
 	print('-+-+-')
 	print(empty_board.get(31,'')+'|'+empty_board.get(32,'')+'|'+empty_board.get(33,''))
 	print('-+-+-')
-	
+
+def winning(status):
+		if (theBoard.get(11,'') != ' ' and (theBoard.get(11,'') == theBoard.get(12,'') and theBoard.get(11,'') == theBoard.get(13,'')) or 
+		(theBoard.get(21,'') != ' ' and theBoard.get(21,'') == theBoard.get(22,'') and theBoard.get(21,'') == theBoard.get(23,'')) or 
+		(theBoard.get(31,'') != ' ' and theBoard.get(31,'') == theBoard.get(32,'') and theBoard.get(31,'') == theBoard.get(33,'')) or 
+		(theBoard.get(11,'') != ' ' and theBoard.get(11,'') == theBoard.get(21,'') and theBoard.get(11,'') == theBoard.get(31,'')) or 
+		(theBoard.get(12,'') != ' ' and theBoard.get(12,'') == theBoard.get(22,'') and theBoard.get(12,'') == theBoard.get(32,'')) or 
+		(theBoard.get(13,'') != ' ' and  theBoard.get(13,'') == theBoard.get(23,'') and theBoard.get(13,'') == theBoard.get(33,''))) :
+			print (str(turn)+ " WON THE GAME ")
+			return 1
